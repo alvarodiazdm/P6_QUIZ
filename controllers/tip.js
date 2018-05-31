09ff9155658bca0da76c8327ec2e445f6d90bdf4
Alvaro Diaz del Mazo <alvarodiazdm@gmail.com>
2018-05-31 16:17:20 +0200

    Preparando rama p8

--- controllers/tip.js
+++ controllers/tip.js
@@ -20,12 +20,14 @@ exports.load = (req, res, next, tipId) => {
 
 // POST /quizzes/:quizId/tips
 exports.create = (req, res, next) => {
- 
+
+    const authorId = req.session.user && req.session.user.id || 0;
+
     const tip = models.tip.build(
         {
             text: req.body.text,
             quizId: req.quiz.id,
-            author: authorId
+            authorId: authorId
         });
 
     tip.save()
